7828b1c2310129236922e94156a9480d23c2eab1
Михаил Сергеев <server-15@yandex.ru>
2023-01-20 20:53:55 +0500

    Выпадающее меню

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 FROM python:3.9.16-alpine3.17
 
+
 RUN mkdir /app
+RUN addgroup djos && adduser -G djos -s /bin/sh -D djos
+RUN chown -R djos:djos /app
+USER djos
 WORKDIR /app
 COPY requirements.txt ./
 RUN pip install --upgrade pip && pip install -r requirements.txt
-CMD ["/bin/sh"]
+CMD ["sh"]
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-# DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+# DEBUG = False
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = [
     'www.djos.pythonanywhere.com',
